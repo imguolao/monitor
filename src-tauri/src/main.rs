9c672ce45989d1f3f693d22cb4f464b236f29a6e
imguolao <guolao.mq@foxmail.com>
2024-12-17 10:58:04 +0800

    build: update tauri to LTS

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-mod utils;
-mod ddcci;
-mod wmi;
-mod monitor;
-mod command;
-
-use command::{get_monitor_list, set_monitor_brightness};
-
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![get_monitor_list, set_monitor_brightness])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    monitor_lib::run()
 }
